0cf763bdcddce61bd39eb1ab0c706e0019bf0ad2
Albert Hunt <albert.hunt10@gmail.com>
2026-02-10 17:32:39 -0600

    sync activities drilled to activity item but bug on sync

--- src/activities/ActivityItem.jsx
+++ src/activities/ActivityItem.jsx
@@ -1,12 +1,19 @@
 import { useAuth } from "../auth/AuthContext";
-export default function ActivityItem({ activity }) {
+export default function ActivityItem({ activity, syncActivities }) {
   const { token } = useAuth();
   const { deleteActivity } = useAuth();
   return (
     <>
       <li>{activity.name}</li>
       {!token ? null : (
-        <button onClick={() => deleteActivity(activity.id)}>Delete</button>
+        <button
+          onClick={() => {
+            deleteActivity(activity.id);
+            syncActivities();
+          }}
+        >
+          Delete
+        </button>
       )}
     </>
   );
